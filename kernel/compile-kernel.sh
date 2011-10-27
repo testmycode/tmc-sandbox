37e67381ef5182a9155aed7e40800aabdeb14014
Martin Pärtel <martin.partel@gmail.com>
2011-10-27 13:30:51 +0300

    Added FPU register fix to UML kernel. Seems to fix Java crashes.

--- kernel/compile-kernel.sh
+++ kernel/compile-kernel.sh
@@ -17,6 +17,7 @@ if [ ! -d linux-$VERSION ]; then
   patch -p1 < ../aufs3-standalone/aufs3-base.patch
   patch -p1 < ../aufs3-standalone/aufs3-proc_map.patch
   patch -p1 < ../aufs3-standalone/aufs3-standalone.patch
+  patch -p1 < ../fpu-fix.patch
   cp -av ../aufs3-standalone/Documentation/* Documentation/
   cp -av ../aufs3-standalone/fs/* fs/
   cp -av ../aufs3-standalone/include/linux/aufs_type.h include/linux/
