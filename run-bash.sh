40b35218d22c325f56de309b54c126f8396b2e3c
Martin Pärtel <martin.partel@gmail.com>
2011-11-04 18:08:22 +0200

    Changed output to be a tar file, for which trailing nulls don't matter.

--- run-bash.sh
+++ run-bash.sh
@@ -11,9 +11,13 @@ chmod +x tmp/tmc-run
 
 tar -C tmp -cf tmp/bash-runner.tar tmc-run || exit 1
 
+MAX_OUTPUT_SIZE=20M
+dd if=/dev/zero of=tmp/output.tar bs=$MAX_OUTPUT_SIZE count=1
+
 output/linux.uml \
   initrd=output/initrd.img \
   ubda=output/rootfs.squashfs \
   ubdb=tmp/bash-runner.tar \
+  ubdc=tmp/output.tar \
   mem=128M
 
--- run-test-exercise.sh
+++ run-test-exercise.sh
@@ -3,17 +3,16 @@
 rm -Rf tmp
 mkdir -p tmp
 tar -C test/data/test-exercise -cf tmp/test-exercise.tar . || exit 1
-gcc -o tmp/remove-trailing-nulls test/helpers/remove-trailing-nulls.c || exit 1
 
 MAX_OUTPUT_SIZE=20M
-dd if=/dev/zero of=tmp/output.txt0 bs=$MAX_OUTPUT_SIZE count=1
+dd if=/dev/zero of=tmp/output.tar bs=$MAX_OUTPUT_SIZE count=1
 
 output/linux.uml \
   initrd=output/initrd.img \
   ubda=output/rootfs.squashfs \
   ubdb=tmp/test-exercise.tar \
-  ubdc=tmp/output.txt0 \
+  ubdc=tmp/output.tar \
   mem=128M
 
-tmp/remove-trailing-nulls < tmp/output.txt0 > tmp/output.txt
+tar -C tmp -xf tmp/output.tar
 
