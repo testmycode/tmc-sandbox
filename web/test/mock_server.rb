0c628a8a3d5e31519e55aada8320669429875d02
Martin Pärtel <martin.partel@gmail.com>
2011-11-08 12:22:41 +0200

    Web: ruby 1.8.7 fix

--- web/test/mock_server.rb
+++ web/test/mock_server.rb
@@ -7,61 +7,80 @@ class MockServer
   end
 
   def interact(&block)
-    IO.pipe do |pipe_in, pipe_out|
-      server_pid = Process.fork do
-        $stdin.close
-        $stdout.close
-        $stderr.close
-        pipe_in.close
-        
-        app = Rack::Builder.new do
-          map '/ready' do
-            ready = lambda do
-              [200, {'Content-Type' => 'text/plain'}, ["ready"]]
-            end
-            run ready
-          end
-          
-          map '/notify' do
-            notify = lambda do |env|
-              req = Rack::Request.new(env)
-              pipe_out.write(MultiJson.encode({:content_type => env['CONTENT_TYPE'], :params => req.params}))
-              pipe_out.close
-              [200, {'Content-Type' => 'text/plain'}, ["OK"]]
-            end
-            run notify
-          end
-        end.to_app
-        
-        Rack::Handler::WEBrick.run(app, :Host => 'localhost', :Port => 11988, :Logger => WEBrick::Log.new('/dev/null'))
-      end
-      
+    @req_data = nil
+    
+    pipe_block do |pipe_in, pipe_out|
+      server_pid = fork_server_process(pipe_in, pipe_out)
       pipe_out.close
       
-      req_data = nil
-      reader = Thread.fork do
-        req_data = MultiJson.decode(pipe_in.read)
-      end
-      
-      while true
-        begin
-          res = Net::HTTP.get(URI('http://localhost:11988/ready'))
-          break
-        rescue Errno::ECONNREFUSED
-          sleep 0.1
-        end
-      end
-      
       begin
+        reader = Thread.fork do
+          @req_data = MultiJson.decode(pipe_in.read)
+        end
+      
+        wait_for_server_to_be_ready
         block.call
       ensure
-        reader.join
+        reader.join if reader
         
         Process.kill("KILL", server_pid)
         Process.waitpid(server_pid)
-        
-        return req_data
       end
+    end
+    
+    @req_data
+  end
+  
+private
+  def fork_server_process(pipe_in, pipe_out)
+    Process.fork do
+      $stdin.close
+      $stdout.close
+      $stderr.close
+      pipe_in.close
+      
+      app = Rack::Builder.new do
+        map '/ready' do
+          ready = lambda do
+            [200, {'Content-Type' => 'text/plain'}, ["ready"]]
+          end
+          run ready
+        end
+        
+        map '/notify' do
+          notify = lambda do |env|
+            req = Rack::Request.new(env)
+            pipe_out.write(MultiJson.encode({:content_type => env['CONTENT_TYPE'], :params => req.params}))
+            pipe_out.close
+            [200, {'Content-Type' => 'text/plain'}, ["OK"]]
+          end
+          run notify
+        end
+      end.to_app
+      
+      Rack::Handler::WEBrick.run(app, :Host => 'localhost', :Port => 11988, :Logger => WEBrick::Log.new('/dev/null'))
+    end
+  end
+  
+  def wait_for_server_to_be_ready
+    while true
+      begin
+        Net::HTTP.get(URI('http://localhost:11988/ready'))
+        break
+      rescue Errno::ECONNREFUSED
+        sleep 0.1
+      end
+    end
+  end
+  
+  def pipe_block(&block)
+    # IO.pipe with a block not supported on Ruby 1.8.7
+    infd, outfd = IO.pipe
+    begin
+      block.call(infd, outfd)
+    ensure
+      infd.close unless infd.closed?
+      outfd.close unless outfd.closed?
     end
   end
 end
