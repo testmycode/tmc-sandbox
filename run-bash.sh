9878f1b9f13dae0768c95297d7ca25a0ed2fa0f8
Martin Pärtel <martin.partel@gmail.com>
2012-02-14 12:19:13 +0200

    Made run-bash.sh read MEM envvar.

--- run-bash.sh
+++ run-bash.sh
@@ -18,11 +18,13 @@ cd ..
 MAX_OUTPUT_SIZE=20M
 dd if=/dev/zero of=tmp/output.tar bs=$MAX_OUTPUT_SIZE count=1
 
+MEM=${MEM-96M}
+
 output/linux.uml \
   initrd=output/initrd.img \
   ubdarc=output/rootfs.squashfs \
   ubdbr=tmp/bash-runner.tar \
   ubdc=tmp/output.tar \
-  mem=96M \
+  mem=$MEM \
   tmc_run_bash
 
