2c622130d4a06bd71bd5afb80574738d8f263e38
Martin Pärtel <martin.partel@gmail.com>
2012-07-09 21:00:57 +0300

    Removed old management script.

--- uml/management/all_tests.rb
+++
@@ -1,8 +0,0 @@
-lambda do
-  testdir = File.dirname(File.realpath(__FILE__))
-  tests = Dir.entries(testdir).select {|e| e.end_with?('_test.rb') }
-  tests.sort!
-  for test in tests
-    require "#{testdir}/#{test}"
-  end
-end.call
