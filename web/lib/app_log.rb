a00dfc82798ead2e662007c385f1a3729c9d5364
Martin Pärtel <martin.partel@gmail.com>
2012-08-08 13:37:09 +0300

    Fix logging.
    Kernel#warn was being called instead of AppLog.method_missing.

--- web/lib/app_log.rb
+++ web/lib/app_log.rb
@@ -11,6 +11,31 @@ class AppLog
     @log = logger
   end
 
+  def self.debug(msg)
+    get.debug(msg)
+  end
+
+  def self.info(msg)
+    get.info(msg)
+  end
+
+  def self.warn(msg)
+    get.warn(msg)
+  end
+
+  def self.error(msg)
+    get.error(msg)
+  end
+
+  def self.fatal(msg)
+    get.fatal(msg)
+  end
+
+  def self.unknown(msg)
+    get.unknown(msg)
+  end
+
+
   def self.method_missing(name, *args, &block)
     self.get.send(name, *args, &block)
   end
