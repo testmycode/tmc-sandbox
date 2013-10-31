3493eccab532b250119c62c9ecf8df7a75a849c1
Martin Pärtel <martin.partel@gmail.com>
2013-10-31 22:57:40 +0200

    Updated kernel to 3.11.6.

--- uml/build-scripts/compile-kernel.sh
+++ uml/build-scripts/compile-kernel.sh
@@ -7,14 +7,10 @@ if [ ! -d fs/aufs ]; then
   patch -p1 < ../aufs3-standalone/aufs3-kbuild.patch
   patch -p1 < ../aufs3-standalone/aufs3-base.patch
   patch -p1 < ../aufs3-standalone/aufs3-proc_map.patch
-  patch -p1 < ../aufs3-standalone/aufs3-standalone.patch
   cp -av ../aufs3-standalone/Documentation/* Documentation/
   cp -av ../aufs3-standalone/fs/* fs/
   cp -av ../aufs3-standalone/include/linux/aufs_type.h include/linux/
-  
-  # custom patches
-  patch -p1 < ../../kernel/readonly-ubd-fix.patch
-  patch -p1 < ../../kernel/um-pass-through-siginfo.patch
+  cp -av ../aufs3-standalone/include/uapi/linux/aufs_type.h include/uapi/linux/
 fi
 
 if [ `uname -i` = i386 ]; then
