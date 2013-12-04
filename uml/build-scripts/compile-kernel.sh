38d6a124e0bb8be93040f87f77e622cf85ea765a
Martin Pärtel <martin.partel@gmail.com>
2013-12-04 08:57:43 +0200

    Kernel to 3.11.10 and fix aufs build.

--- uml/build-scripts/compile-kernel.sh
+++ uml/build-scripts/compile-kernel.sh
@@ -6,10 +6,9 @@ cd $KERNEL_DIR
 if [ ! -d fs/aufs ]; then
   patch -p1 < ../aufs3-standalone/aufs3-kbuild.patch
   patch -p1 < ../aufs3-standalone/aufs3-base.patch
-  patch -p1 < ../aufs3-standalone/aufs3-proc_map.patch
+  patch -p1 < ../aufs3-standalone/aufs3-mmap.patch
   cp -av ../aufs3-standalone/Documentation/* Documentation/
   cp -av ../aufs3-standalone/fs/* fs/
-  cp -av ../aufs3-standalone/include/linux/aufs_type.h include/linux/
   cp -av ../aufs3-standalone/include/uapi/linux/aufs_type.h include/uapi/linux/
 fi
 
