18e705b1945c42099f1aeec94c600604ff2f1b55
Jarmo Isotalo <jamo@isotalo.fi>
2015-11-28 19:49:44 +0200

    Added travis.yml

---
+++ .setup-web-for-travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Configures and builds web api for travis
+
+cd web/
+bundle install
+rake ext
+sed -i "s/\(tmc_user: \)tmc/\1 $(whoami)/" site.defaults.yml
+sed -i "s/\(tmc_group: \)tmc/\1 $(whoami)/" site.defaults.yml
+sed -i 's/\(max_instances: \)[0-9]*/\12/' site.defaults.yml
+cd ../
+
+
