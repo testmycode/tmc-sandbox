dfe51d79e1309d83390eeda53beee012d1fa07f1
Jarmo Isotalo <jamo@isotalo.fi>
2015-12-04 23:23:56 +0200

    Travis

---
+++ .setup-sandbox-for-travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Builds sandbox
+curl -L http://ftp-master.debian.org/archive-key-6.0.asc | sudo apt-key add -
+# Multistrap is broken so let's fix it - see: https://bugs.launchpad.net/ubuntu/+source/multistrap/+bug/1313787
+sudo sed -i -e '989s/$forceyes//' /usr/sbin/multistrap
+# To workaround travis timeouts
+echo "Builing sandbox, it will take a while (upto 30 minutes)"
+while sleep 1; do echo -n "."; done &
+sudo make > /tmp/build.log 2>&1
+kill %1
---
+++ .setup-web-for-travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Configures and builds web api for travis
+
+cd web/
+bundle install  --retry=6 --jobs=3
+rake ext
+sed -i "s/\(tmc_user: \)tmc/\1 $(whoami)/" site.defaults.yml
+sed -i "s/\(tmc_group: \)tmc/\1 $(whoami)/" site.defaults.yml
+sed -i 's/\(max_instances: \)[0-9]*/\12/' site.defaults.yml
+cd ../
+
